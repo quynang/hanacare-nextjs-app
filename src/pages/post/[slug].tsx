715483e8172c272ca879ce1a1a6f9f6106a28205
pmc-quynang <nang.nq@pharmacity.vn>
2023-02-13 23:38:47 +0700

    Bugfix

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -8,7 +8,7 @@ export default function PostDetail() {
   const url = `https://hanacare.vn/ghost/api/content/posts/slug/${slug}/?key=942efd06374ce7156d0bf617c4&include=tags`;
   const fetcher = async () => await axios.get(url).then((res) => res.data);
 
-  const { data, error } = useSWR(url, fetcher)
+  const { data, error } = useSWR(slug ? url : null, fetcher)
 
   if (error) return <div>Failed to load</div>
   if (!data) return null
