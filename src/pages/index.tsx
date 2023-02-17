a546d32be2a2a3dd70a462e98f668f3efad001b2
pmc-quynang <nang.nq@pharmacity.vn>
2023-02-17 13:27:07 +0700

    HomePage: Section for your employees

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,12 @@ const sectionTopics = [
   { image: '/images/topic.svg', title: 'Chăm sóc sức khoẻ tiện lợi cùng Đối tác MiniApp' }
 ]
 
+const sectionForYourEmployees = [
+  { image: '/images/mobile-feature-1.jpg', title: 'Đánh giá chỉ số hạnh phúc' },
+  { image: '/images/mobile-feature-2.jpeg', title: 'Tổ chức hoạt động đội nhóm' },
+  { image: '/images/mobile-feature-3.jpeg', title: 'Báo cáo chỉ số ENPS' }
+]
+
 export default function HomePage() {
   const url = `https://hanacare.vn/ghost/api/content/posts?key=942efd06374ce7156d0bf617c4&limit=6&include=tags,authors`;
   const fetcher = async () => await axios.get(url).then((res) => res.data);
@@ -28,7 +34,7 @@ export default function HomePage() {
       <main>
         <div className="container mx-auto">
           <HeroSection />
-          <section className="topics-area py-20 xl:py-28">
+          <section className="topics-area pt-20 xl:pt-28">
             <div className="container mx-auto">
               <div className="section-title text-center max-w-xl mx-auto mb-6 lg:mb-16">
                 <h4 className="font-serif text-4xl lg:text-5xl text-gray-800 font-bold mb-6">Trải nghiệm cùng HanaCare</h4>
@@ -41,12 +47,29 @@ export default function HomePage() {
               </div>
             </div>
           </section>
+          <section className="for-your-employees-area py-20 xl:py-28">
+            <div className="container mx-auto">
+              <div className="section-title text-center max-w-xl mx-auto">
+                <h4 className="font-serif text-4xl lg:text-5xl text-gray-800 font-bold mb-6">Dành cho nhân viên của bạn</h4>
+                <p className="text-lg text-gray-400 font-normal">Tạo điều kiện và cung cấp cho nhân viên của bạn cùng tham gia vào các chương trình, hoạt động nhằm nâng cao và hỗ trợ tất cả các nhu cầu về sức khỏe của họ</p>
+              </div>
+              <div className="grid grid-cols-3 gap-4 mb-8">
+                {sectionForYourEmployees.map((item, index) => (
+                  <div key={index} className="relative">
+                    <img src={item.image} />
+                    <div className='absolute backdrop-blur m-2 p-4 bottom-0 left-0 w-full text-center text-xl text-gray-600 font-bold'>
+                      {item.title}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </section>
           <section className="articles-area">
             <div className="section-title text-center max-w-xl mx-auto mb-6 lg:mb-16">
               <h4 className="font-serif text-4xl lg:text-5xl text-gray-800 font-bold mb-6">Blog HanaCare</h4>
               <p className="text-lg text-gray-600 font-normal">Khám phá các bài viết về những thông tin bổ ích đến sức khoẻ và các thói quen lành mạnh hàng ngày</p>
             </div>
-
             <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-8 posts'>
               {
                 posts.map((post: any, index: number) => <PostCard key={index} post={post} />)
