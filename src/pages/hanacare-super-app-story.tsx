8223408bd0da1588c883fddec61006ec862d6318
pmc-quynang <nang.nq@pharmacity.vn>
2023-03-06 23:06:40 +0700

    Cosmetic response UI and update resource

--- src/pages/hanacare-super-app-story.tsx
+++ src/pages/hanacare-super-app-story.tsx
@@ -1,76 +1,114 @@
-import TopicCard from "@/components/cards/TopicCard"
+import TopicCard from '@/components/cards/TopicCard';
 
 const contentBoxs = [
   {
     icon: '/images/tam-nhin.png',
     title: 'Tầm nhìn',
-    description: 'Mỗi người được sử dụng dịch vụ và sản phẩm CSSK tốt nhất để họ có cuộc sống hạnh phúc trọn vẹn'
+    description:
+      'Mỗi người được sử dụng dịch vụ và sản phẩm CSSK tốt nhất để họ có cuộc sống hạnh phúc trọn vẹn',
   },
   {
     icon: '/images/su-menh.png',
     title: 'Sứ mệnh',
-    description: 'Cung cấp ứng dụng CSSK dành cho mọi người để giải quyết vấn đề một cách tiện lợi'
+    description:
+      'Cung cấp ứng dụng CSSK dành cho mọi người để giải quyết vấn đề một cách tiện lợi',
   },
   {
     icon: '/images/gia-tri-cot-loi.png',
     title: 'Giá trị cốt lõi',
-    description: 'Integrity - Gốc rễ vấn đề - Data driven - Khoẻ mạnh - Truyền cảm hứng'
-  }
-]
+    description:
+      'Integrity - Gốc rễ vấn đề - Data driven - Khoẻ mạnh - Truyền cảm hứng',
+  },
+];
 
 const HanacareSuperAppStory = () => {
   return (
     <>
-      <div className="hero-area  relative pt-10 xl:pt-20 pb-10 xl:pb-20 ">
-        <div className="container mx-auto">
-          <div className="hero-box-3 relative grid gap-8 grid-cols-1 lg:grid-cols-12 text-left">
-            <div className="lg:col-span-8">
-              <div className="hero-content lg:flex flex-wrap text-left relative lg:pr-14">
-                <h1 className="w-full !leading-tight text-4xl lg:text-5xl text-gray-800 font-bold  mb-6">Câu chuyện về HanaCare - Nguồn cảm hứng khởi đầu</h1>
+      <div className='hero-area  relative pt-10 pb-10 xl:pt-20 xl:pb-20 '>
+        <div className='container mx-auto'>
+          <div className='hero-box-3 relative grid grid-cols-1 gap-8 text-left md:grid-cols-12'>
+            <div className='md:col-span-8'>
+              <div className='hero-content relative flex-wrap text-left lg:flex'>
+                <h1 className='mb-6 w-full text-4xl font-bold !leading-tight text-gray-800  lg:text-5xl'>
+                  Câu chuyện về HanaCare - Nguồn cảm hứng khởi đầu
+                </h1>
               </div>
             </div>
-            <div className="lg:col-span-4">
-              <div className="feature-image relative aos-init aos-animate" data-aos="fade" data-aos-delay="100">
-                <div className="rounded-xl bg-left-top w-full bg-[url('/images/hanacare-founder.jpg')]" style={{ height: 272, backgroundSize: '100% 160%', backgroundPosition: '0px -50px' }} />
+            <div className='md:col-span-4'>
+              <div
+                className='feature-image aos-init aos-animate relative'
+                data-aos='fade'
+                data-aos-delay='100'
+              >
+                <img
+                  className='rounded-xl'
+                  src='/images/hanacare-founder.jpg'
+                />
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="single-are pb-20">
-        <div className='gh-content gh-canvas pb-20 xl:pb-20 max-w-4xl mx-auto'>
+      <div className='single-are pb-20'>
+        <div className='gh-content gh-canvas mx-auto max-w-4xl pb-20 xl:pb-20'>
           <p>
-            Vào giữa năm 2021, khi mà covid-19 đang hoành hành, đau thương mất mát xảy ra ở khắp nơi, khiến nhiều người hoang mang tuyệt vọng. Lúc đó hình ảnh các thiên thần áo trắng, chiến đấu quên bản thân chăm sóc cho mọi người đã cho tôi niềm tin vào tương lai, rằng dịch bệnh sẽ qua đi, mọi người sẽ lại có cuộc sống an yên.
+            Vào giữa năm 2021, khi mà covid-19 đang hoành hành, đau thương mất
+            mát xảy ra ở khắp nơi, khiến nhiều người hoang mang tuyệt vọng. Lúc
+            đó hình ảnh các thiên thần áo trắng, chiến đấu quên bản thân chăm
+            sóc cho mọi người đã cho tôi niềm tin vào tương lai, rằng dịch bệnh
+            sẽ qua đi, mọi người sẽ lại có cuộc sống an yên.
           </p>
           <p>
-            Và cũng từ đó, ý tưởng về xây dựng app chăm sóc sức khỏe và hạnh phúc cho mọi người được hình thành. Tôi muốn xây dựng một ứng dụng như thiên thần áo trắng bên cạnh mọi người, cung cấp những dịch vụ chăm sóc sức khỏe đa dạng và tiện lợi.
+            Và cũng từ đó, ý tưởng về xây dựng app chăm sóc sức khỏe và hạnh
+            phúc cho mọi người được hình thành. Tôi muốn xây dựng một ứng dụng
+            như thiên thần áo trắng bên cạnh mọi người, cung cấp những dịch vụ
+            chăm sóc sức khỏe đa dạng và tiện lợi.
           </p>
           <p>
-            Từ tư vấn sức khỏe đến chăm sóc bệnh nhân, đặt hẹn với bác sĩ. Từ giảm cân, tập gym đến điều trị tâm lý và tăng cường sức khỏe tinh thần. Từ bảo hiểm, quản lý tài chính đến việc kết nối cộng đồng. Giúp xây dựng một môi trường trong lành và phát triển bản thân.
+            Từ tư vấn sức khỏe đến chăm sóc bệnh nhân, đặt hẹn với bác sĩ. Từ
+            giảm cân, tập gym đến điều trị tâm lý và tăng cường sức khỏe tinh
+            thần. Từ bảo hiểm, quản lý tài chính đến việc kết nối cộng đồng.
+            Giúp xây dựng một môi trường trong lành và phát triển bản thân.
           </p>
           <p>
-            Tôi muốn ứng dụng này có thể giúp mọi người có một cuộc sống an yên trọn vẹn.
+            Tôi muốn ứng dụng này có thể giúp mọi người có một cuộc sống an yên
+            trọn vẹn.
           </p>
           <p>
-            Sau khi ghi chép lại ý tưởng xong, tôi lập tức chia sẻ ý tưởng với các thành viên trong đội  Mọi người nhanh chóng đồng thuận vì mỗi thành viên, ai cũng đang ấp ủ xây dựng được một ứng dụng có thể đóng góp cho cộng đồng đặc biệt là về vấn đề sức khỏe. Chính vì thế, mà chúng tôi bắt tay vào sắp xếp những viên gạch đầu tiên để xây dựng ứng dụng ngay.
+            Sau khi ghi chép lại ý tưởng xong, tôi lập tức chia sẻ ý tưởng với
+            các thành viên trong đội Mọi người nhanh chóng đồng thuận vì mỗi
+            thành viên, ai cũng đang ấp ủ xây dựng được một ứng dụng có thể đóng
+            góp cho cộng đồng đặc biệt là về vấn đề sức khỏe. Chính vì thế, mà
+            chúng tôi bắt tay vào sắp xếp những viên gạch đầu tiên để xây dựng
+            ứng dụng ngay.
           </p>
           <p>
-            Hana có nghĩa là hạnh phúc cũng có nghĩa là hoa. Hoa tặng người mình yêu quý, hoa chữa lành vết thương người được tặng. Chúng tôi muốn xây dựng HanaCare thành một ứng dụng biết chăm sóc, chia sẻ, quan tâm mọi người và được mọi người yêu mến. Vậy nên chúng tôi đã đặt tên app là HanaCare, app chăm sóc sức khỏe và hạnh phúc cho mọi người.
+            Hana có nghĩa là hạnh phúc cũng có nghĩa là hoa. Hoa tặng người mình
+            yêu quý, hoa chữa lành vết thương người được tặng. Chúng tôi muốn
+            xây dựng HanaCare thành một ứng dụng biết chăm sóc, chia sẻ, quan
+            tâm mọi người và được mọi người yêu mến. Vậy nên chúng tôi đã đặt
+            tên app là HanaCare, app chăm sóc sức khỏe và hạnh phúc cho mọi
+            người.
           </p>
-          <div className="flex justify-end">
-            <p className="italic">Đạt Nguyễn - Founder</p>
+          <div className='flex justify-end'>
+            <p className='italic'>Nguyễn Hữu Đạt - Founder</p>
           </div>
         </div>
       </div>
-      <div className="container mx-auto">
-        <div className="grid sm:grid-col-2 md:grid-cols-3 mb-8 gap-8">
+      <div className='container mx-auto'>
+        <div className='sm:grid-col-2 mb-8 grid gap-8 md:grid-cols-3'>
           {contentBoxs.map((item, index) => (
-            <TopicCard key={index} title={item.title} description={item.description} image={item.icon} />
+            <TopicCard
+              key={index}
+              title={item.title}
+              description={item.description}
+              image={item.icon}
+            />
           ))}
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default HanacareSuperAppStory
+export default HanacareSuperAppStory;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,22 +3,25 @@ import Layout from '@/components/layout/Layout';
 import Seo from '@/components/Seo';
 import HeroSection from '@/components/layout/HeroSection';
 import PostCard from '@/components/cards/PostCard';
-import useSWR from 'swr'
-import axios from "axios";
+import useSWR from 'swr';
+import axios from 'axios';
 import TopicCard from '@/components/cards/TopicCard';
 import { features } from '@/config';
 
 const sectionForYourEmployees = [
   { image: '/images/mobile-feature-1.png', title: 'Đánh giá chỉ số hạnh phúc' },
-  { image: '/images/mobile-feature-2.png', title: 'Tổ chức hoạt động đội nhóm' },
-  { image: '/images/mobile-feature-3.png', title: 'Báo cáo chỉ số ENPS' }
-]
+  {
+    image: '/images/mobile-feature-2.png',
+    title: 'Tổ chức hoạt động đội nhóm',
+  },
+  { image: '/images/mobile-feature-3.png', title: 'Báo cáo chỉ số ENPS' },
+];
 
 export default function HomePage() {
   const url = `https://ghost.hanacare.vn/ghost/api/content/posts?key=942efd06374ce7156d0bf617c4&limit=6&include=tags,authors`;
   const fetcher = async () => await axios.get(url).then((res) => res.data);
   const { data, error } = useSWR(url, fetcher);
-  const { posts = [] } = data || {}
+  const { posts = [] } = data || {};
 
   if (error) <p>Loading failed...</p>;
   if (!data) <h1>Loading...</h1>;
@@ -27,50 +30,83 @@ export default function HomePage() {
     <Layout>
       <Seo />
       <main>
-        <div className="container mx-auto">
+        <div className='container mx-auto'>
           <HeroSection />
-          <section className="topics-area pt-20 xl:pt-28">
-            <div className="container mx-auto">
-              <div className="section-title text-center max-w-xl mx-auto mb-6 lg:mb-16">
-                <h4 className="text-4xl lg:text-5xl text-gray-800 font-bold mb-6">Trải nghiệm cùng HanaCare</h4>
-                <p className="text-lg text-gray-600 font-normal">Chúng tôi cung cấp nội dung sức khoẻ phù hợp vào đúng thời điểm</p>
+          <section className='topics-area pt-20 xl:pt-28'>
+            <div className='container mx-auto'>
+              <div className='section-title mx-auto mb-6 max-w-xl text-center lg:mb-16'>
+                <h4 className='mb-6 text-4xl font-bold text-gray-800 lg:text-5xl'>
+                  Trải nghiệm cùng HanaCare
+                </h4>
+                <p className='text-lg font-normal text-gray-600'>
+                  Chúng tôi cung cấp nội dung sức khoẻ phù hợp vào đúng thời
+                  điểm
+                </p>
               </div>
-              <div className="flex justify-center flex-wrap">
+              <div className='flex flex-wrap justify-center'>
                 {features.map((item, index) => (
-                  <div key={index} className='m-4 w-full md:w-[calc(33.33%-2rem)] lg:w-[calc(25%-2rem)] '>
+                  <div
+                    key={index}
+                    className='m-4 w-full md:w-[calc(33.33%-2rem)]'
+                  >
                     <TopicCard title={item.title} image={item.image} />
                   </div>
                 ))}
               </div>
             </div>
           </section>
-          <section className="for-your-employees-area py-20 xl:py-28">
-            <div className="container mx-auto">
-              <div className="section-title text-center max-w-xl mx-auto">
-                <h4 className="text-4xl lg:text-5xl text-gray-800 font-bold mb-6">Dành cho nhân viên của bạn</h4>
-                <p className="text-lg text-gray-600 font-normal">Tạo điều kiện và cung cấp cho nhân viên của bạn cùng tham gia vào các chương trình, hoạt động nhằm nâng cao và hỗ trợ tất cả các nhu cầu về sức khỏe của họ</p>
+          <section className='for-your-employees-area py-20 xl:py-28'>
+            <div className='container mx-auto'>
+              <div className='section-title mx-auto max-w-xl text-center'>
+                <h4 className='mb-6 text-4xl font-bold text-gray-800 lg:text-5xl'>
+                  Dành cho nhân viên của bạn
+                </h4>
+                <p className='text-lg font-normal text-gray-600'>
+                  Tạo điều kiện và cung cấp cho nhân viên của bạn cùng tham gia
+                  vào các chương trình, hoạt động nhằm nâng cao và hỗ trợ tất cả
+                  các nhu cầu về sức khỏe của họ
+                </p>
               </div>
-              <div className="grid md:grid-cols-3 gap-4">
+              <div className='relative grid gap-4 md:grid-cols-3'>
                 {sectionForYourEmployees.map((item, index) => (
-                  <div key={index} className="flex flex-col justify-center relative p-10">
-                    <img style={{ width: 'auto', maxHeight: 350, objectFit: 'contain' }} src={item.image} />
-                    <div className='mt-6 w-full text-center text-xl text-gray-800 font-bold'>
+                  <div
+                    key={index}
+                    className='relative flex flex-col justify-center p-10'
+                  >
+                    <img
+                      style={{
+                        width: 'auto',
+                        maxHeight: 350,
+                        objectFit: 'contain',
+                      }}
+                      src={item.image}
+                    />
+                    <div className='mt-6 w-full text-center text-xl font-bold text-gray-800'>
                       {item.title}
                     </div>
                   </div>
                 ))}
+                <img
+                  className='absolute top-1/2 left-1/2 -z-10 hidden -translate-x-1/2 -translate-y-1/2 transform md:block'
+                  src='/images/wave.svg'
+                ></img>
               </div>
             </div>
           </section>
-          <section className="articles-area">
-            <div className="section-title text-center max-w-xl mx-auto mb-6 lg:mb-16">
-              <h4 className="text-4xl lg:text-5xl text-gray-800 font-bold mb-6">Blog HanaCare</h4>
-              <p className="text-lg text-gray-600 font-normal">Khám phá các bài viết về những thông tin bổ ích đến sức khoẻ và các thói quen lành mạnh hàng ngày</p>
+          <section className='articles-area'>
+            <div className='section-title mx-auto mb-6 max-w-xl text-center lg:mb-16'>
+              <h4 className='mb-6 text-4xl font-bold text-gray-800 lg:text-5xl'>
+                Blog HanaCare
+              </h4>
+              <p className='text-lg font-normal text-gray-600'>
+                Khám phá các bài viết về những thông tin bổ ích đến sức khoẻ và
+                các thói quen lành mạnh hàng ngày
+              </p>
             </div>
-            <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-8 posts'>
-              {
-                posts.map((post: any, index: number) => <PostCard key={index} post={post} />)
-              }
+            <div className='posts grid gap-8 sm:grid-cols-2 lg:grid-cols-3'>
+              {posts.map((post: any, index: number) => (
+                <PostCard key={index} post={post} />
+              ))}
             </div>
           </section>
         </div>
