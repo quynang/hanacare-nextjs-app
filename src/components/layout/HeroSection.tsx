7cfdd953cbebdd762fe38c9e81b2f743a4f25d73
pmc-quynang <nang.nq@pharmacity.vn>
2023-04-26 01:40:00 +0700

    Add link download app and update title on hero section

--- src/components/layout/HeroSection.tsx
+++ src/components/layout/HeroSection.tsx
@@ -8,12 +8,11 @@ const HeroSection = () => {
         <div className='hero-box-1 grid items-center gap-24 sm:grid-cols-2 lg:grid-cols-12'>
           <div className='hero-cotent relative lg:col-span-7'>
             <h1 className='mb-12 font-serif text-4xl font-bold !leading-tight text-gray-800 lg:text-5xl xl:text-6xl'>
-              Chào mừng đến với &nbsp;
               <span style={{ display: 'inline-block' }}>
                 <span style={{ color: '#75c905' }}>Hana</span>
                 <span style={{ color: '#00b6fd' }}>Care&nbsp;</span>
               </span>
-              - Ứng dụng rèn luyện sức khoẻ cho mọi người
+              - Ứng dụng rèn luyện và theo dõi sức khoẻ
             </h1>
             <h4 className='mb-12 text-xl font-normal text-gray-600'>
               Với mục đích Mọi người đều khoẻ mạnh và hạnh phúc, chúng tôi sẽ
@@ -21,8 +20,18 @@ const HeroSection = () => {
               của cộng đồng
             </h4>
             <div className='fade-in flex w-full justify-start'>
-              <AppStore width='auto' className='mr-4 h-12 w-auto' />
-              <PlayStore width='auto' className='h-12 w-auto' />
+              <a
+                href='https://apps.apple.com/us/app/hanacare/id1670385640'
+                target='_blank'
+              >
+                <AppStore width='auto' className='mr-4 h-12 w-auto' />
+              </a>
+              <a
+                href='https://play.google.com/store/apps/details?id=com.hanacare'
+                target='_blank'
+              >
+                <PlayStore width='auto' className='h-12 w-auto' />
+              </a>
             </div>
           </div>
           <div className='feature-image relative lg:col-span-5'>
