01f4e7b8930ddcc9ab1233cc2fa5132636f296ab
pmc-quynang <nang.nq@pharmacity.vn>
2023-06-08 22:44:33 +0700

    Use content from Ghost admin for privacy page

--- src/pages/chinh-sach-bao-mat-thong-tin.tsx
+++ src/pages/chinh-sach-bao-mat-thong-tin.tsx
@@ -1,71 +1,37 @@
-export default function ChinhSachBaoMatThongTin() {
+import axios from 'axios';
 
+export default function ChinhSachBaoMatThongTin(props: any) {
+  const page = props.data.pages[0];
   return (
     <>
-      <div className="hero-area relative pt-20 xl:pt-28 " >
-        <div className="container mx-auto" >
-          <div className="page-title-box" >
-            <div className="section-title gh-canvas max-w-4xl mx-auto  mb-20" >
-              <h1 className="w-full text-4xl lg:text-5xl text-gray-800 font-bold mb-8" >Chính sách bảo mật cho Ứng dụng HanaCare</h1 >
+      <div className='hero-area relative pt-20 xl:pt-28 '>
+        <div className='container mx-auto'>
+          <div className='page-title-box'>
+            <div className='section-title gh-canvas mx-auto mb-20  max-w-4xl'>
+              <h1 className='mb-8 w-full text-4xl font-bold text-gray-800 lg:text-5xl'>
+                {page.title}
+              </h1>
             </div>
           </div>
         </div>
       </div>
-      <section className="single-area pb-20">
-        <div className="gh-content gh-canvas  max-w-4xl mx-auto">
-          <p>
-            Chúng tôi tôn trọng quyền riêng tư của người dùng và cam kết bảo vệ thông tin cá nhân của bạn. Chính sách bảo mật này mô tả cách chúng tôi thu thập, sử dụng và tiết lộ thông tin cá nhân của bạn khi sử dụng Ứng dụng HanaCare. Vui lòng đọc kỹ chính sách này trước khi sử dụng Ứng dụng.
-          </p>
-          <h3 id='title-1'>
-            1. Thông tin chúng tôi thu thập
-          </h3 >
-          <p>
-            Chúng tôi có thể thu thập thông tin cá nhân của bạn, bao gồm tên, địa chỉ email, số điện thoại và địa chỉ. Chúng tôi cũng có thể thu thập thông tin về sức khỏe của bạn, bao gồm chiều cao, cân nặng, lịch sử bệnh án và thuốc đang dùng.
-          </p>
-          <h3 id='title-2'>
-            2. Mục đích sử dụng thông tin
-          </h3 >
-          <p>
-            Chúng tôi sử dụng thông tin cá nhân của bạn để cung cấp các dịch vụ của Ứng dụng HanaCare, bao gồm tư vấn sức khỏe, lịch trình chăm sóc sức khỏe và gợi ý thực phẩm và chế độ ăn uống. Chúng tôi cũng sử dụng thông tin của bạn để liên lạc với bạn và cải thiện Ứng dụng.
-          </p>
-          <h3 id='title-3'>
-            3. Chia sẻ thông tin cá nhân
-          </h3 >
-          <p>
-            Chúng tôi không chia sẻ thông tin cá nhân của bạn với bất kỳ bên thứ ba nào, trừ khi có sự cho phép của bạn hoặc yêu cầu của pháp luật. Chúng tôi có thể chia sẻ thông tin phi cá nhân với các bên thứ ba để cải thiện dịch vụ của chúng tôi.
-          </p>
-          <h3 id='title-4'>
-            4. Bảo mật thông tin cá nhân
-          </h3 >
-          <p>
-            Chúng tôi cam kết bảo vệ thông tin cá nhân của bạn và sử dụng các biện pháp bảo mật thích hợp để ngăn chặn truy cập trái phép, sử dụng hoặc tiết lộ thông tin cá nhân của bạn.
-          </p>
-          <h3 id='title-5'>
-            5. Lưu trữ thông tin cá nhân
-          </h3 >
-          <p>
-            Chúng tôi lưu trữ thông tin cá nhân của bạn trên các máy chủ an toàn tại các vị trí được quản lý và bảo vệ chặt chẽ.
-          </p>
-          <h3 id='title-6'>
-            6. Thay đổi chính sách bảo mật
-          </h3 >
-          <p>
-            Chúng tôi có thể thay đổi hoặc cập nhật Chính sách bảo mật này bất kỳ lúc nào. Chúng tôi sẽ cập nhật phiên bản mới nhất của Chính sách bảo mật này trên trang web của mình hoặc thông qua Ứng dụng. Bằng cách tiếp tục sử dụng Ứng dụng sau khi thay đổi được áp dụng, bạn đồng ý với các điều khoản và điều kiện mới.
-          </p>
-          <h3 id='title-7'>
-            7. Sự đồng ý của người dùng
-          </h3 >
-          <p>
-            Bằng cách sử dụng Ứng dụng HanaCare, bạn đồng ý với Chính sách bảo mật này và việc thu thập, sử dụng và tiết lộ thông tin cá nhân của bạn theo những điều khoản được nêu trong Chính sách bảo mật này.
-          </p>
-          <h3 id='title-8'>
-            8. Liên hệ với chúng tôi
-          </h3 >
-          <p>
-            Nếu bạn có bất kỳ câu hỏi hoặc ý kiến liên quan đến Chính sách bảo mật của chúng tôi, vui lòng liên hệ với chúng tôi bằng cách gửi email đến địa chỉ hỗ trợ của chúng tôi.
-          </p>
-        </div>
+      <section className='single-area pb-20'>
+        <div
+          className='gh-content gh-canvas  mx-auto max-w-4xl'
+          dangerouslySetInnerHTML={{ __html: page.html }}
+        />
       </section>
     </>
-  )
+  );
+}
+
+export async function getServerSideProps(context: any) {
+  const url = `https://ghost.hanacare.vn/ghost/api/content/pages/slug/chinh-sach-bao-mat-thong-tin?key=942efd06374ce7156d0bf617c4`;
+  const fetcher = async () => await axios.get(url).then((res) => res.data);
+  const data = await fetcher();
+  return {
+    props: {
+      data,
+    },
+  };
 }
